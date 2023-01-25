40e3e79ead833a7068da904cc559d7c979a7afbd
SaverioCascelli <saverio.cascelli@gmail.com>
2023-01-25 17:06:10 +0100

    [add] many to many museum artist from artwork

--- app/Models/Artist.php
+++ app/Models/Artist.php
@@ -10,19 +10,26 @@ class Artist extends Model
 {
     use HasFactory;
 
-    public function artworks(){
+    public function artworks()
+    {
         return $this->hasMany(Artwork::class);
     }
 
-    public static function generateSlug($string){
+    public function museums()
+    {
+        return $this->belongsToMany(Museum::class);
+    }
+
+    public static function generateSlug($string)
+    {
         $slug = Str::slug($string, '-');
 
         $original_slug = $slug;
         $c = 1;
-        $museum_exists = Artist::where('slug',$slug)->first();
-        while($museum_exists){
+        $museum_exists = Artist::where('slug', $slug)->first();
+        while ($museum_exists) {
             $slug = $original_slug . '-' . $c;
-            $museum_exists = Artist::where('slug',$slug)->first();
+            $museum_exists = Artist::where('slug', $slug)->first();
             $c++;
         }
         return $slug;
