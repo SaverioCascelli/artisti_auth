31d8d834f0c14867cac0a4547fe58bee61253135
MarBozzz <mbozzuto@libero.it>
2023-01-25 17:43:00 +0100

    [added] create

--- app/Http/Controllers/Admin/ArtworkController.php
+++ app/Http/Controllers/Admin/ArtworkController.php
@@ -26,7 +26,7 @@ class ArtworkController extends Controller
      */
     public function create()
     {
-        //
+        return view('artworks.create');
     }
 
     /**
@@ -46,9 +46,9 @@ class ArtworkController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Artwork $artwork)
     {
-        //
+        return view('artworks.show', compact('artwork'));
     }
 
     /**
--- resources/views/artworks/index.blade.php
+++ resources/views/artworks/index.blade.php
@@ -1,20 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.admin')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Artist - Artwork</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <!-- Styles -->
-    @vite('resources/js/app.js')
-
-</head>
-
+@section('content')
 <body class="bg-dark vh-100 d-flex justify-content-center flex-column align-items-center">
     <h1 class="text-center text-white mb-5">
         ARTWORKS DB
@@ -42,19 +28,22 @@
                                 <td class="text-white">{{ $artwork->name }}</td>
                                 <td class="text-white">{{ $artwork->year }}</td>
                                 <td>
-                                    <a class="btn btn-success" href="">Vai</a>
+                                    <a class="btn btn-success" href="{{route('admin.artwork.show',$artwork)}}">Vai</a>
                                     <a class="btn btn-warning" href="">Edita</a>
                                     <form action="" method="POST" class="d-inline"
                                         onsubmit="return confirm('Confermi l\'eliminazione di: {{ $artwork->name }}?')"
                                         class="d-inline" action="" method="POST">
-
                                         @csrf
                                         @method('DELETE')
                                         <button type="submit" class="btn btn-danger">Elimina</button>
                                     </form>
                                 </td>
                             </tr>
+
                         @endforeach
+                        <div class="d-flex justify-content-center">
+                            <a class="btn btn-warning mb-5" href="{{route('admin.artwork.create')}}">Create New Artwork</a>
+                          </div>
                     </tbody>
                 </table>
             </div>
@@ -63,3 +52,4 @@
 </body>
 
 </html>
+@endsection
